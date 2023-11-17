0678aacf8a10933e2e9effa2fc82b7c3737a5a9c
Pakatip Kasai <kasai.pakatip@gmail.com>
2023-11-17 14:02:02 +0900

    Leave out unnecessary code

--- data-lake/spark-essentials/rdd_song_lower_case.py
+++ data-lake/spark-essentials/rdd_song_lower_case.py
@@ -1,19 +1,17 @@
 ### 
-# You might have noticed this code in the screencast.
 #
 # import findspark
 # findspark.init('spark-2.3.2-bin-hadoop2.7')
 #
 # The findspark Python module makes it easier to install
-# Spark in local mode on your computer. This is convenient
-# for practicing Spark syntax locally. 
-# However, the workspaces already have Spark installed and you do not
-# need to use the findspark module
+# Spark in local mode on your computer.
 #
 ###
 
 from pyspark.sql import SparkSession
 
+#creating a SparkSession, 
+# which is the entry point to programming Spark with the DataFrame API.
 # Because we aren't running on a spark cluster, the session is just for development
 spark = SparkSession \
     .builder \
@@ -44,22 +42,26 @@ distributed_song_log_rdd = spark.sparkContext.parallelize(log_of_songs)
 # to be collected from ALL the nodes, which kills productivity, and could crash
 # .foreach() allows the data to stay on each of the independent nodes
 
-# show the original input data is preserved
-distributed_song_log_rdd.foreach(print)
+# foreach() function is used to print each element in the RDD distributed_song_log_rdd
+#print("Type of rdd is", type(distributed_song_log_rdd))
+#distributed_song_log_rdd.foreach(print)
 
+#create function that converts a given song title to lowercase.
 def convert_song_to_lowercase(song):
     return song.lower()
 
-print(convert_song_to_lowercase("Havana"))
+#print(convert_song_to_lowercase("Havana"))
+
+
 
 
 # toDF() Converts from an RDD to a DataFrame- this allows us to use convenient functions like show() 
-lower_case_songs=distributed_song_log_rdd.map(convert_song_to_lowercase)
-lower_case_songs.foreach(print)
+#lower_case_songs=distributed_song_log_rdd.map(convert_song_to_lowercase)
+#lower_case_songs.foreach(print)
 
 # Show the original input data is still mixed case
-distributed_song_log_rdd.foreach(print)
+#distributed_song_log_rdd.foreach(print)
 
 # Use lambda functions instead of named functions to do the same map operation
 distributed_song_log_rdd.map(lambda song: song.lower()).foreach(print)
-distributed_song_log_rdd.map(lambda x: x.lower()).foreach(print)
+#distributed_song_log_rdd.map(lambda x: x.lower()).foreach(print)
